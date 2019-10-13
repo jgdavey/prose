7f4df62ff55e0dff7cde2f8730413a7b78272358
Joshua Davey <josh@joshuadavey.com>
2019-10-12 20:15:46 -0400

    Use criterion for benchmarking

---
+++ benches/benchmark.rs
@@ -0,0 +1,25 @@
+#[macro_use]
+extern crate criterion;
+
+use criterion::Criterion;
+use criterion::black_box;
+use prose::{Reformatter, FormatOpts};
+
+fn bench_reformatting(c: &mut Criterion) {
+    c.bench_function("analysis", |b| {
+        let data = include_str!("../tests/data/inputs/plain.txt");
+        let opts = FormatOpts::with_max_length(40);
+
+        b.iter(|| Reformatter::new(&opts, black_box(data)));
+    });
+    c.bench_function("reformat", |b| {
+        let data = include_str!("../tests/data/inputs/plain.txt");
+        let opts = FormatOpts::with_max_length(40);
+        let reformatter = black_box(Reformatter::new(&opts, data));
+
+        b.iter(|| reformatter.reformatted());
+    });
+}
+
+criterion_group!(benches, bench_reformatting);
+criterion_main!(benches);
